4def21465353f674e8d749e06c2e0120ee2321b2
pnijhara <prajjwalnijhara@gmail.com>
2019-04-02 23:31:40 +0530

    Add photo.js

---
+++ html_stream/photo.js
@@ -0,0 +1,24 @@
+(function(){
+	var video = document.getElementById('video'),
+		vendorURL = window.URL || window.webkitURL;
+	navigator.getUserMedia = navigator.getUserMedia ||
+                             navigator.webkitGetUserMedia ||
+                             navigator.mozGetUserMedia;
+
+if (navigator.getUserMedia) {
+   navigator.getUserMedia({ audio: true, video: { width: 1280, height: 720 } },
+      function(stream) {
+         var video = document.querySelector('video');
+         video.srcObject = stream;
+         video.onloadedmetadata = function(e) {
+           video.play();
+         };
+      },
+      function(err) {
+         console.log("The following error occurred: " + err.name);
+      }
+   );
+} else {
+   console.log("getUserMedia not supported");
+}
+}) ();
